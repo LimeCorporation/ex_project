e551ac60199dce48abb99f8c9f50809a62955468
Imputes <zhuzizhuzi1995@gmail.com>
2018-05-15 14:37:28 +0800

    Add the Playing_C folder

---
+++ Playing_C/cmake-build-debug/CMakeFiles/Playing_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Playing_C.dir/main.c.o"
+  "Playing_C.pdb"
+  "Playing_C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Playing_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
